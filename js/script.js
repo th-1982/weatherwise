647bb77acb872b273758bbad5877c1d70b871055
Theresa Wolff <theresawolff@gmail.com>
2024-04-26 23:06:59 +0200

    Update weather icon based on weather condition

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
 
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
@@ -14,6 +15,22 @@ async function checkWeather(city){
     document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
     document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
     document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if(data.weather[0].main == "Clouds"){
+        weatherIcon.src = "images/clouds.png";
+    }
+    else if(data.weather [0].main == "Clear"){
+        weatherIcon.src = "images/clear.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        weatherIcon.src = "images/rain.png";
+    }
+    else if (data.weather[0].main == "Drizz"){
+        weatherIcon.src = "images/drizzle.png";
+    }
+    else if (data.weather[0].main == "Mist"){
+        weatherIcon.src = "images/mist.png";
+    }
 }
 
 
